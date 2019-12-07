1fee7283a7d2cc3bb12daf9d393bda71a2d7ff8b
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 06:34:17 -0600

    Add files via upload

--- JS/CreateNavigation.js
+++ JS/CreateNavigation.js
@@ -1,12 +1,7 @@
 function createNavigation(data) {
-    console.log(data);
-    var container = document.getElementById("navigation");;
     [].forEach.call(data.split("\n"), function(line) {
-        fields = line.split(",");
-        var title = fields[0];
-        var icon = fields[1];
-        console.log(title);
-        console.log(icon);
-        container.insertAdjacentHTML('beforeend', '<label title="'+title+'" class="fas fa-'+icon+'"><input type="radio" name="menu" id="'+title+'" onclick="pages(this.id)"/></label>');
+        var [title, icon] = line.split(",");
+        document.getElementById("navigation").insertAdjacentHTML('beforeend', '\
+            <label title="'+title+'" class="fas fa-'+icon+'"><input type="radio" name="menu" id="'+title+'" onclick="pages(this.id)"/></label>');
     });
 }
