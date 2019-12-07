f4722fee83aa9b6c4e09c26ca535bebc4b93f154
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 17:29:43 -0600

    Add files via upload

---
+++ JS/CreatePages.js
@@ -0,0 +1,11 @@
+function createAboutPage(data) {
+    var first = true;
+    var pre;
+    [].forEach.call(data.split("\n"), function(line) {
+        if (first){
+            document.querySelector('main').insertAdjacentHTML('beforeend', '<section><h2>'+line+'</h2></section>');
+            first=false;
+        } else pre +=line+' ';
+    });
+    document.querySelector('main > section').insertAdjacentHTML('beforeend', '<pre>'+pre+'</pre>');
+}
