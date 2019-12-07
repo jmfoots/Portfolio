7e031a86e2d7f40f91858b9a84877c8b6c554b90
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 06:47:40 -0600

    Add files via upload

--- JS/NavigationFunctions.js
+++ JS/NavigationFunctions.js
@@ -1,8 +1,3 @@
-function menu() { 
-    var classes = document.querySelector('label[for=menu]').classList;
-    classes.toggle('fa-times'); classes.toggle('fa-bars');
-}
-
 function pages(id) { 
     [].forEach.call(document.querySelectorAll('nav > ul > label'), function(label) {
         if (label != document.querySelector('label[title='+id+']')) {
