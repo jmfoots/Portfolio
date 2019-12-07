ce0a62c055cebb1a8a9a124cad74ac26f5446289
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 13:42:21 -0600

    Add files via upload

--- JS/lastUpdate.js
+++ JS/lastUpdate.js
@@ -1,4 +1,3 @@
 function lastDate() {
-    console.log(document.lastModified);
-    document.querySelector('summary[title=lastDate]').insertAdjacentText('afterend', document.lastModified);
+    document.querySelector('summary[title=lastDate]').insertAdjacentText('beforeend', document.lastModified);
 }
