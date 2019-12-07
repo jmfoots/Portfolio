892c5d0d7342cff6a40a800dda06c27c281b2e29
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 16:46:59 -0600

    Add files via upload

--- JS/NavigationFunctions.js
+++ JS/NavigationFunctions.js
@@ -10,15 +10,15 @@ function pages(id) {
 
 document.addEventListener('keydown', function(e) {
     var children = document.querySelectorAll('nav > label');
-    var current = document.querySelectorAll('nav > label[class*=far]')
+    var current = document.querySelectorAll('label[class*=far]')
     var index = Array.prototype.indexOf.call(children, current);
     if (([39, 9, 38].indexOf(e.keyCode) >= 0)){ //Next
-        console.log(index);
         if (index < children.length) index+=1;
             else index=0;
     } else if (([37, 8, 40].indexOf(e.keyCode) >= 0)){ //Prev
         if (index > 0) index-=1;
             else index=(children.length-1);
     }
-    children.item.indexOf(index).click();
+    console.log(index);
+    document.querySelectorAll('nav > label')[index].click();
 }); 
