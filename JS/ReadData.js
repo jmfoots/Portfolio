ecc69599fca353f8b21b9f7277c81b81025dc47f
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 00:00:13 -0600

    Add files via upload

--- JS/ReadData.js
+++ JS/ReadData.js
@@ -3,7 +3,7 @@ function readData(file) {
     req.open("GET", file, false);
     req.onreadystatechange = function() {
         if(req.readyState === 4 && req.status === 200) {
-            return req.responseText;
-        }
-    }; return "";
+            return console.log(req.responseText);
+        } else return "";
+    }; req.send(null);
 }
