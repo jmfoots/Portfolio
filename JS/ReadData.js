cc121c4fa19b81a1fa9a5e85cfcfc25d4330491d
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 00:25:29 -0600

    Add files via upload

--- JS/ReadData.js
+++ JS/ReadData.js
@@ -1,7 +1,6 @@
 function readData(file) {
     var req = new XMLHttpRequest();
     req.open("GET", file, false);
-    req.responseType = 'text';
     req.onreadystatechange = function() {
         if(req.readyState === req.DONE){
             if (req.status === 200) {
