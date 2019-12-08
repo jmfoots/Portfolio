6086f5d3852b24a31c927ae6cab53cc120cbbe53
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 23:56:48 -0600

    Add files via upload

--- JS/ContentFunctions.js
+++ JS/ContentFunctions.js
@@ -1,6 +1,7 @@
 document.onload(function() {
     document.querySelector('summary[title=\'Page Details\']').insertAdjacentText('beforeend', document.lastModified);
     document.querySelector('nav > label:first-child > input').click();
+    document.querySelector('iframe[title=navigation]');
 });
 
 document.addEventListener('keyup', function(e) {
