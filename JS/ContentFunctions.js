5b16ae791cabb31846b6e59d85dc3cf530c771c6
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 20:21:41 -0600

    Add files via upload

--- JS/ContentFunctions.js
+++ JS/ContentFunctions.js
@@ -1,6 +1,4 @@
 document.addEventListener('keyup', function(e) {
-    var lightbox = document.querySelector('details[title=lightbox]');
-    if (lightbox.open && ([32, 27].indexOf(e.keyCode) >= 0)){
-        document.querySelector('details[title=lightbox]').click();
-    }
+    var lightbox = document.querySelector('details[title=lightbox][open]');
+    if (lightbox && ([32, 27].indexOf(e.keyCode) >= 0)) lightbox.click();
 });
