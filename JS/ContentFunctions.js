6d92368a95aa99daea03b7ad4a1009a52a37250b
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 20:22:33 -0600

    Add files via upload

--- JS/ContentFunctions.js
+++ JS/ContentFunctions.js
@@ -1,4 +1,4 @@
 document.addEventListener('keyup', function(e) {
-    var lightbox = document.querySelector('details[title=lightbox][open]');
+    var lightbox = document.querySelector('details[title=lightbox][open] > summary');
     if (lightbox && ([32, 27].indexOf(e.keyCode) >= 0)) lightbox.click();
 });
