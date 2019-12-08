443934e7bff944f247172ec7f812f17f6856efda
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-08 08:38:07 -0600

    Add files via upload

--- JS/ManageContent.js
+++ JS/ManageContent.js
@@ -16,8 +16,8 @@ function init(){
                     else index=children.length-1;
             }
             console.log(index);
-            document.querySelectorAll('nav > label')[index].dispatchEvent(new Event('click'), new Event('touchend'));
-        } else if (lightbox && ([32, 27].indexOf(e.keyCode) >= 0)) lightbox.dispatchEvent(new Event('click'), new Event('touchend'));
+            document.querySelectorAll('nav > label')[index].click();
+        } else if (lightbox && ([32, 27].indexOf(e.keyCode) >= 0)) lightbox.click();
     });
 }
 
