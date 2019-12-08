f290ce82b80ebae70f07c322c95bc2af8bdd9dd0
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 21:14:42 -0600

    Add files via upload

--- JS/CreatePages.js
+++ JS/CreatePages.js
@@ -5,8 +5,8 @@ function createAboutPage(data) {
         if (first){
             var script = "document.querySelector(\'details[title=lightbox] > summary\').click()";
             document.querySelector('main').insertAdjacentHTML('beforeend', `
-                <section>
-                    <h2 title="Show ${line}" onclick="${script}">${line}</h2>
+                <section title=grid>
+                        <h2 title="Show ${line}" onclick="${script}">${line}</h2>
                 </section>`);
             first=false;
         } else pre+=line+' ';
