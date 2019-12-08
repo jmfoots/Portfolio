a44ae73015c96f9ffd930e6756c98690f9d598f1
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-07 23:50:19 -0600

    Add files via upload

--- JS/ContentFunctions.js
+++ JS/ContentFunctions.js
@@ -1,4 +1,9 @@
+document.onload(function() {
+    document.querySelector('summary[title=\'Page Details\']').insertAdjacentText('beforeend', document.lastModified);
+});
+
 document.addEventListener('keyup', function(e) {
     var lightbox = document.querySelector('details[title=lightbox][open] > summary');
     if (lightbox && ([32, 27].indexOf(e.keyCode) >= 0)) lightbox.click();
 });
+
