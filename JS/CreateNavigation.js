2a39a2c7daee415599d93b64d9b92f073317edfd
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-08 08:26:48 -0600

    Delete CreateNavigation.js

--- JS/CreateNavigation.js
+++
@@ -1,9 +0,0 @@
-function createNavigation(data) {
-    [].forEach.call(data.split("\n"), function(line) {
-        var [title, icon] = line.split(",");
-        document.querySelector('nav').insertAdjacentHTML('beforeend', `
-            <label title="${title}" class="fas fa-${icon}">
-                <input type="radio" name="menu" id="${title}" onclick="pages(this.id)"/>
-            </label>`);
-    });
-}
