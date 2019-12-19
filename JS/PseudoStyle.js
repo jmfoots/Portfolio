c8cf802236853f0328321503500da0fa12727abb
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-19 13:01:40 -0600

    Add files via upload

--- JS/PseudoStyle.js
+++ JS/PseudoStyle.js
@@ -6,11 +6,11 @@ HTMLElement.prototype.pseudoStyle = function(element,prop,value){
 	var css = document.getElementById(id) || document.createElement('style');
       css.id = id;
       sheet = css.innerHTML;
-  var style = sheet.indexOf(`${this}::${element}{`);
+  var style = sheet.indexOf(`${this.tagName}::${element}{`);
   if (style > 0) {
-    sheet.splice(style,`\n${prop}:${value};`);
+    sheet.splice(style,`\n${prop}:${value}`);
   } else {
-    sheet += `${this}::${element}{${prop}:${value};}`
+    sheet += ` ${this.tagName}::${element}{${prop}:${value}}`
     document.getElementsByTagName('head')[0].appendChild(css);
   }
   console.log(sheet);
