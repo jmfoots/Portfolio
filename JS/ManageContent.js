15857853c1565930f6f71de77272c108214dfe3f
jmfoots <50424689+jmfoots@users.noreply.github.com>
2019-12-19 11:56:58 -0600

    Add files via upload

--- JS/ManageContent.js
+++ JS/ManageContent.js
@@ -78,11 +78,11 @@ function hexify(){
             border-right: ${horizontal}px solid transparent;
             bottom: calc(1px - ${vertical}px);`;
         [].forEach.call(before.split("\n"), function(css) {
-            [attr, style]=line.split(":");
+            [attr, style]=css.split(":");
             h.pseudoStyle("before", attr, style);
         });
         [].forEach.call(after.split("\n"), function(css) {
-            [attr, style]=line.split(":");
+            [attr, style]=css.split(":");
             h.pseudoStyle("after", attr, style);
         });
     });
